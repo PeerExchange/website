0684fc58f458412697f3f4035c7bfc871ebafbb4
Matthew <80741503+dev-matthew@users.noreply.github.com>
2022-04-26 22:59:12 -0700

    Add files via upload

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -409,12 +409,12 @@ const ORG_ABI = [
 ];
 
 window.onload = function() {
-    //getMetamask();
-    document.getElementById("left-nav").style.display = "none";
+    getMetamask();
+    //document.getElementById("left-nav").style.display = "none";
     //document.getElementById("user-nav").style.display = "block";
-    document.getElementById("right-nav").style.display = "none";
+    //document.getElementById("right-nav").style.display = "none";
 	//document.getElementById("search-nav").style.display = "block";
-	document.getElementById("admin-nav").style.display = "block";
+	//document.getElementById("admin-nav").style.display = "block";
 }
 
 async function getMetamask() {
